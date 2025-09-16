5be6208ade0691bf284d48c2230b274e23ab7e2b
softdevintraining <akilu@bu.edu>
2025-09-16 17:34:27 -0400

    Progress commit

--- restaurant/views.py
+++ restaurant/views.py
@@ -8,6 +8,14 @@ from django.http import HttpRequest, HttpResponse
 import time
 import random
 
+food_prices = {
+    'Wings': [8.0, 14.0],
+    'Chicken Sandwich': [10.0],
+    'Waffle Fries': [6.0],
+    'Texas Toast': [3.0],
+    'Special': [10.0],
+}
+
 # Create your views here.
 def main(request):
     '''Respond to the URLs: '/main\''''
@@ -29,12 +37,46 @@ def order(request):
     }
     return render(request, template_name, context)
 
+# def confirmation(request):
+#     '''Respond to the URL: '/confirmation\''''
+    
+#     template_name = 'restaurant/confirmation.html'
+    
+#     context = {
+#         "time": time.ctime(),
+#     }
+#     return render(request, template_name, context)
+
 def confirmation(request):
-    '''Respond to the URL: '/confirmation\''''
+    '''Process form submission, return HTML result.'''
     
-    template_name = 'restaurant/confirmation.html'
-    
-    context = {
-        "time": time.ctime(),
-    }
+    template_name = "restaurant/order.html"
+    print(request)
+
+    if request.POST:
+        template_name = "restaurant/confirmation.html"
+
+        # extract submitted form fields into variables:
+        items = request.POST.getlist("items")
+        name = request.POST["user_name"]
+        phone = request.POST["user_phone"]
+        email = request.POST["user_email"]
+        special_instructions = request.POST["special_instructions"]
+        
+        print(items)
+        print(name)
+        print(phone)
+        print(email)
+        print(special_instructions)
+
+        # add variables to context
+        context = {
+            'time': time.ctime(),
+            'items': items,
+            'name': name,
+            'phone': phone,
+            'email': email,
+            'special_instructions': special_instructions or "No instructions provided.",
+        }
+
     return render(request, template_name, context)
