b530f0bb788472cf27042c5d49b3efcb1315ec34
softdevintraining <akilu@bu.edu>
2025-09-16 22:49:24 -0400

    Bit more work done, still gotta figure out how to do random time properly and start/finish css

--- restaurant/views.py
+++ restaurant/views.py
@@ -9,12 +9,24 @@ import time
 import random
 
 food_prices = {
-    'Wings': [8.0, 14.0],
-    'Chicken Sandwich': [10.0],
-    'Waffle Fries': [6.0],
-    'Texas Toast': [3.0],
-    'Special': [10.0],
+    '8 Pc. Wings': [12.0, 14.0],
+    'Chicken Sandwich': 10.0,
+    'Waffle Fries': 6.0,
+    'Texas Toast': 3.0,
+    'Slice of Apple Pie': 7.0,
+    'Loaded Fries': 10.0,
+    'K-BBQ Tenders': 15.0,
+    'Impossible Chicken Wings': 12.0,
 }
+
+daily_specials = [
+    'Apple Pie',
+    'Loaded Fries',
+    'K-BBQ Tenders',
+    'Impossible Chicken Wings',
+]
+
+special = None
 
 # Create your views here.
 def main(request):
@@ -31,21 +43,13 @@ def order(request):
     '''Respond to the URL: '/order\''''
 
     template_name = 'restaurant/order.html'
+    special = daily_specials[random.randint(0, len(daily_specials) - 1)]
 
     context = {
         "time": time.ctime(),
+        "special": special,
     }
     return render(request, template_name, context)
-
-# def confirmation(request):
-#     '''Respond to the URL: '/confirmation\''''
-    
-#     template_name = 'restaurant/confirmation.html'
-    
-#     context = {
-#         "time": time.ctime(),
-#     }
-#     return render(request, template_name, context)
 
 def confirmation(request):
     '''Process form submission, return HTML result.'''
@@ -62,11 +66,22 @@ def confirmation(request):
         phone = request.POST["user_phone"]
         email = request.POST["user_email"]
         special_instructions = request.POST["special_instructions"]
-        
+
+        price = 0.0
+        for item in items:
+            if item == "8 Pc. Wings":
+                if request.POST.get("wings_type") == "boneless":
+                    price += food_prices[item][0]
+                else:
+                    price += food_prices[item][1]
+            else:
+                price += food_prices[item]
+
         print(items)
         print(name)
         print(phone)
         print(email)
+        print(price)
         print(special_instructions)
 
         # add variables to context
@@ -76,6 +91,7 @@ def confirmation(request):
             'name': name,
             'phone': phone,
             'email': email,
+            'price': price,
             'special_instructions': special_instructions or "No instructions provided.",
         }
 
