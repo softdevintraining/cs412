391b4728316f39003e8a8194c7dc8d470b2ece8e
softdevintraining <akilu@bu.edu>
2025-09-24 17:30:00 -0400

    Able to render profiles properly for both list and detail views. Finishing up css

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -8,4 +8,5 @@ from . import views
 # URL patterns for this app:
 urlpatterns = [
     path(r'', views.ProfileListView.as_view(), name="show_all_profiles"),
+    path(r'profile/<int:pk>', views.ProfileDetailView.as_view(), name="show_profile"),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Profile
 
 import random
@@ -10,3 +10,7 @@ class ProfileListView(ListView):
     template_name = "mini_insta/show_all_profiles.html"
     context_object_name = "profiles"
 
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = "mini_insta/show_profile.html"
+    context_object_name = "profile"
