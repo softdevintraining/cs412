7f73a5fbc199ebe29b966a1fdff166d1ff6682dc
softdevintraining <akilu@bu.edu>
2025-10-06 23:30:12 -0400

    finished up example 5

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 # blog/views.py
 # views for the blog applicaiton
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from .models import Article
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Article, Comment
 from .forms import CreateArticleForm, CreateCommentForm, UpdateArticleForm
 from django.urls import reverse
 
@@ -107,3 +107,22 @@ class UpdateArticleView(UpdateView):
     model = Article
     form_class = UpdateArticleForm
     template_name = "blog/update_article_form.html"
+
+class DeleteCommentView(DeleteView):
+    '''View class to delete a comment on an Article.'''
+
+    model = Comment
+    template_name = "blog/delete_comment_form.html"
+
+    def get_success_url(self):
+        '''Return the URL to redirect to after a succesful delete.'''
+
+        # find the PK for this comment
+        pk = self.kwargs['pk']
+        # find the Comment object:
+        comment = Comment.objects.get(pk=pk)
+
+        # find the PK of the article
+        article = comment.article
+
+        return reverse('article', kwargs={'pk':article.pk})
